7f0fc5b4705407b1ceb59a663b8785946638eeff
Justin Choo <kchoosun@gmail.com>
2020-10-24 16:59:38 -0700

    added App component

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import App from './App';
+
+// uncomment so that webpack can bundle styles
+// import styles from './scss/application.scss';
+
+render(<App />, document.getElementById('root'));
